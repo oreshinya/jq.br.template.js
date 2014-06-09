8cd0c76308d238d50156a9274695a600225dcb36
shinya takahashi <s.takahashi313@gmail.com>
2014-06-09 22:36:26 +0900

    remove

--- src/jq.br.template.js
+++ src/jq.br.template.js
@@ -9,7 +9,6 @@
 
 
   (function() {
-    var cache = {};
     this.tmpl = function tmpl(str, data) {
       var fn = !/\W/.test(str)||/.js$/.test(str) ? tmpl(views[str]) : new Function("obj", "var p=[],print=function(){p.push.apply(p,arguments);};" + "with(obj){p.push('" + str.replace(/[\r\t\n]/g, " ").replace(/'(?=[^%]*%>)/g, "\t").split("'").join("\\'").split("\t").join("'").replace(/<%=(.+?)%>/g, "',$1,'").split("<%").join("');").split("%>").join("p.push('") + "');}return p.join('');");
       return data ? fn(data) : fn;
